9557a9b2b8bd67d5a0be5b89e6dd8d4a8ad292c9
sonbeo1999hd <43996384+sonbeo1999hd@users.noreply.github.com>
2019-05-06 14:57:33 +0700

    update model

---
+++ Nguyễn Mạnh Thắng/model.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const user = new Schema({
+  email: String,
+  name: String,
+  birthday: Date,
+  introduce: String,
+  accessToken: String,
+  gender: String,
+  age: {
+    type: Number,
+    default: 18
+  },
+  school: String,
+  avatarUrl: Array,
+  contact: {
+    type: String,
+    default: null
+  },
+  fbId: String,
+  createdAt: {
+    type: Date,
+    default: new Date().toUTCString()
+  },
+  OTP: {
+    type: String,
+    required: true,
+    default: "000000"
+  },
+  verify: {
+    type: Boolean,
+    required: true,
+    default: false
+  },
+  Like: [{ id: String, date: Date }],
+  Liked: [{ id: String, date: Date }],
+});
+const UserModel = mongoose.model('User', user);
+module.exports = UserModel;
